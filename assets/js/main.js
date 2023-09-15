8f8dcf7e1345a197fd6886b074dff261a9790009
“LiciaLicari” <licianbsplicari@gmail.com>
2023-09-15 15:45:24 +0200

    fix @click

--- assets/js/main.js
+++ assets/js/main.js
@@ -167,16 +167,17 @@ createApp({
                     ],
                 }
             ],
-            methods: {
-                showActiveContact(i) {
-                    console.log(this.activeContact);
-                    console.log(i);
-                    this.activeContact = i;
-                    console.log(this.activeContact);
-                    console.log(i);
-                    return `activeChat ${this.activeContact === index - 1 ? 'active' : ''}`
-                }
-            }
+            
+        }
+    },
+    methods: {
+        showActiveContact(i) {
+            console.log(this.activeContact);
+            console.log(i);
+            this.activeContact = i;
+            console.log(this.activeContact);
+            console.log(i);
+            return true;
         }
     }
 }).mount('#app')
